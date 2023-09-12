e119121950c9807de98e1e11893042e973403d8e
Elina Chertova <elas.2015@mail.ru>
2023-09-12 16:31:10 +0300

    binaries

---
+++ cmd/server/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/elina-chertova/metrics-alerting.git/cmd/server/flags"
+	"github.com/elina-chertova/metrics-alerting.git/cmd/server/handlers"
+	"github.com/elina-chertova/metrics-alerting.git/cmd/storage"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	flags.ParseServerFlags()
+	router := gin.Default()
+	s := &storage.MemStorage{
+		Gauge:   make(map[string]float64),
+		Counter: make(map[string]int64),
+	}
+	router.POST("/update/:metricType/:metricName/:metricValue", handlers.MetricsHandler(s))
+	router.GET("/value/:metricType/:metricName", handlers.GetMetricsHandler(s))
+	router.GET("/", handlers.MetricsListHandler(s))
+	router.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "Page not found")
+	})
+	return router.Run(flags.FlagAddress)
+}
