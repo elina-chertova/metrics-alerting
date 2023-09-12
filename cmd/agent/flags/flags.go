e27f798cf546c7cf658ffc59e9ad4ed0769a37fe
Elina Chertova <elas.2015@mail.ru>
2023-09-12 16:08:32 +0300

    flags

--- cmd/agent/flags/flags.go
+++ cmd/agent/flags/flags.go
@@ -10,7 +10,7 @@ var PollInterval time.Duration
 var ReportInterval time.Duration
 
 func ParseAgentFlags() {
-	flag.StringVar(&FlagAddress, "a", ":8080", "address and port to run server")
+	flag.StringVar(&FlagAddress, "a", "localhost:8080", "address and port to run server")
 	flag.DurationVar(&PollInterval, "p", 2*time.Second, "time in seconds to update metrics, example: 2s")
 	flag.DurationVar(&ReportInterval, "r", 10*time.Second, "time in seconds to send data to server, example: 10s")
 	flag.Parse()
