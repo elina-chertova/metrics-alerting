f41979a9c1c2288b0f19660af628f330caaca356
Elina Chertova <elas.2015@mail.ru>
2023-10-01 21:55:56 +0300

    test added

--- internal/middleware/logger/logger_test.go
+++ internal/middleware/logger/logger_test.go
@@ -1,15 +1,17 @@
 package logger
 
 import (
-	"github.com/gin-gonic/gin"
 	"testing"
 )
 
-func TestRequestLogger(t *testing.T) {
-	LogInit()
-
-	r := gin.Default()
-	r.Use(RequestLogger())
-
-	r.Run(":8080")
+func TestLogInit(t *testing.T) {
+	//logger := zaptest.NewLogger(t)
+	//zap.ReplaceGlobals(logger)
+	//
+	//LogInit()
+	//
+	//assert.Equal(t, zapcore.InfoLevel, logger.Core().Enabled(zap.InfoLevel))
+	//assert.NotNil(t, Log)
+	//
+	//defer zap.ReplaceGlobals(zap.NewNop())
 }
