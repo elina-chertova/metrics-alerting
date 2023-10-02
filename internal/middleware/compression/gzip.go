50579d5267938dd178cdd0215df78786aae2eaae
Elina Chertova <elas.2015@mail.ru>
2023-10-02 03:20:59 +0300

    json handlers changed

--- internal/middleware/compression/gzip.go
+++ internal/middleware/compression/gzip.go
@@ -1,37 +1,44 @@
 package compression
 
-//
-//type gzipWriter struct {
-//	gin.ResponseWriter
-//	Writer io.Writer
-//}
-//
-//func (w gzipWriter) Write(b []byte) (int, error) {
-//	return w.Writer.Write(b)
-//}
-//
-//func GzipHandle() gin.HandlerFunc {
-//	return func(c *gin.Context) {
-//		if !strings.Contains(
-//			c.Request.Header.Get("Accept-Encoding"),
-//			"gzip",
-//		) && c.Request.Method != http.MethodGet {
-//			c.Next()
-//			return
-//		}
-//
-//		gz, err := gzip.NewWriterLevel(c.Writer, gzip.BestSpeed)
-//		if err != nil {
-//			io.WriteString(c.Writer, err.Error())
-//			return
-//		}
-//		defer gz.Close()
-//
-//		c.Header("Content-Encoding", "gzip")
-//		c.Writer = gzipWriter{ResponseWriter: c.Writer, Writer: gz}
-//		c.Next()
-//	}
-//}
+import (
+	"compress/gzip"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type gzipWriter struct {
+	gin.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func GzipHandle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if !strings.Contains(
+			c.Request.Header.Get("Accept-Encoding"),
+			"gzip",
+		) && c.Request.Method != http.MethodGet {
+			c.Next()
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(c.Writer, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(c.Writer, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		c.Header("Content-Encoding", "gzip")
+		c.Writer = gzipWriter{ResponseWriter: c.Writer, Writer: gz}
+		c.Next()
+	}
+}
 
 //func LengthHandle(w http.ResponseWriter, r *http.Request) {
 //	// создаём *gzip.Reader, который будет читать тело запроса
